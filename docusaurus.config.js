53ff8366024ce5ad66fac515d8e33e108994025f
Kurpanik <71767601+Kurpanik@users.noreply.github.com>
2021-03-18 08:55:09 +0100

    Replace double quotation marks with single in configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,50 +1,50 @@
 module.exports = {
-  title: "GLDF",
-  tagline: "Global Luminaire Data Format",
-  url: "https://gldf.io",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "globallightingdata",
-  projectName: "website",
+  title: 'GLDF',
+  tagline: 'Global Luminaire Data Format',
+  url: 'https://gldf.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'globallightingdata',
+  projectName: 'website',
   themeConfig: {
     prism: {
-      defaultLanguage: "xml",
-      additionalLanguages: ["csharp"],
+      defaultLanguage: 'xml',
+      additionalLanguages: ['csharp'],
       /* Themes: https://github.com/FormidableLabs/prism-react-renderer/tree/master/src/themes */
-      theme: require("prism-react-renderer/themes/palenight"),
-      darkTheme: require("prism-react-renderer/themes/palenight"),
+      theme: require('prism-react-renderer/themes/palenight'),
+      darkTheme: require('prism-react-renderer/themes/palenight'),
     },
     navbar: {
-      title: "GLDF",
+      title: 'GLDF',
       logo: {
-        alt: "GLDF Logo",
-        src: "img/logo.png",
-        srcDark: "img/logo-dark.png",
+        alt: 'GLDF Logo',
+        src: 'img/logo.png',
+        srcDark: 'img/logo-dark.png',
       },
       items: [
         {
-          to: "blog/",
-          label: "Blog",
-          position: "left",
+          to: 'blog/',
+          label: 'Blog',
+          position: 'left',
         },
         {
-          to: "getting-started/",
-          label: "Getting Started",
-          position: "left",
+          to: 'getting-started/',
+          label: 'Getting Started',
+          position: 'left',
         },
         {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Docs",
-          position: "left",
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
         },
         {
-          href: "/xsd-reference/index_Root.html",
-          label: "XSD Reference",
-          position: "left",
-          target: "_blank",
+          href: '/xsd-reference/index_Root.html',
+          label: 'XSD Reference',
+          position: 'left',
+          target: '_blank',
         },
         // {
         //   type: 'localeDropdown',
@@ -70,102 +70,102 @@ module.exports = {
         //   ],
         // },
         {
-          to: "download",
-          label: "Download",
-          position: "right",
+          to: 'download',
+          label: 'Download',
+          position: 'right',
         },
         {
-          href: "https://github.com/globallightingdata/gldf",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/globallightingdata/gldf',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "DIAL GmbH",
+          title: 'DIAL GmbH',
           items: [
             {
-              label: "Website",
-              href: "https://www.dial.de",
+              label: 'Website',
+              href: 'https://www.dial.de',
             },
             {
-              label: "DIALux",
-              href: "https://www.dial.de/dialux/",
+              label: 'DIALux',
+              href: 'https://www.dial.de/dialux/',
             },
             {
-              label: "LUMsearch",
-              href: "https://lumsearch.com",
+              label: 'LUMsearch',
+              href: 'https://lumsearch.com',
             },
           ],
         },
         {
-          title: "RELUX Informatik AG",
+          title: 'RELUX Informatik AG',
           items: [
             {
-              label: "Website",
-              href: "https://reluxnet.relux.com",
+              label: 'Website',
+              href: 'https://reluxnet.relux.com',
             },
             {
-              label: "ReluxDesktop",
-              href: "https://reluxnet.relux.com/en/relux-desktop.html",
+              label: 'ReluxDesktop',
+              href: 'https://reluxnet.relux.com/en/relux-desktop.html',
             },
             {
-              label: "ReluxNet",
-              href: "https://reluxnet.relux.com/en/search/luminaires/",
+              label: 'ReluxNet',
+              href: 'https://reluxnet.relux.com/en/search/luminaires/',
             },
           ],
         },
         {
-          title: "Imprint",
+          title: 'Imprint',
           items: [
             {
-              label: "Legal notice",
-              to: "imprint",
+              label: 'Legal notice',
+              to: 'imprint',
             },
             {
-              label: "Data Protection Policy",
-              to: "privacy-policy",
+              label: 'Data Protection Policy',
+              to: 'privacy-policy',
             },
           ],
         },
       ],
       copyright: `
       © ${new Date().getFullYear()} DIAL GmbH & RELUX Informatik AG. All rights reserved.<br />
-      Created with <a href ="https://github.com/facebook/docusaurus" target="_blank">Docusaurus</a>.
+      Created with <a href ='https://github.com/facebook/docusaurus' target='_blank'>Docusaurus</a>.
       `,
     },
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            "https://github.com/globallightingdata/website/edit/develop/",
+            'https://github.com/globallightingdata/website/edit/develop/',
         },
         blog: {
           showReadingTime: true,
           editUrl:
-            "https://github.com/globallightingdata/website/edit/develop/",
+            'https://github.com/globallightingdata/website/edit/develop/',
         },
         theme: {
           customCss: [
-            require.resolve("./src/css/custom.scss"),
-            require.resolve("./src/css/index.scss"),
-            require.resolve("./src/css/imprint.scss"),
+            require.resolve('./src/css/custom.scss'),
+            require.resolve('./src/css/index.scss'),
+            require.resolve('./src/css/imprint.scss'),
           ],
         },
       },
     ],
   ],
-  plugins: ["docusaurus-plugin-sass"],
+  plugins: ['docusaurus-plugin-sass'],
   // scripts: [
   //   {
-  //     src: "https://s.imgur.com/min/embed.js",
+  //     src: 'https://s.imgur.com/min/embed.js',
   //     async: true,
   //   },
   // ],
--- sidebars.js
+++ sidebars.js
@@ -1,20 +1,20 @@
 module.exports = {
   sidebar: {
-    Overview: ["intro"],
-    // "Getting started": [
-    //   "getting-started/concept",
+    Overview: ['intro'],
+    // 'Getting started': [
+    //   'getting-started/concept',
     //   {
     //     ChildCategory: [
-    //       "getting-started/childCategory/download",
-    //       "getting-started/childCategory/doc1",
-    //       "getting-started/childCategory/doc2",
-    //       "getting-started/childCategory/doc3",
+    //       'getting-started/childCategory/download',
+    //       'getting-started/childCategory/doc1',
+    //       'getting-started/childCategory/doc2',
+    //       'getting-started/childCategory/doc3',
     //     ],
     //   },
     // ],
-    // Advanced: ["advanced/hierarchy"],
-    // "3D Geometry": ["geometry/parametrical", "geometry/l3d"],
-    // Sensors: ["sensors/definition"],
-    // Examples: ["examples/simple-luminaire", "examples/complex-luminaire", "examples/sensor-luminaire"],
+    // Advanced: ['advanced/hierarchy'],
+    // '3D Geometry': ['geometry/parametrical', 'geometry/l3d'],
+    // Sensors: ['sensors/definition'],
+    // Examples: ['examples/simple-luminaire', 'examples/complex-luminaire', 'examples/sensor-luminaire'],
   },
 };
