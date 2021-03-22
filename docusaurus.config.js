9c16615b7af42bdaabdec5bf6500071082862dd1
Kurpanik <71767601+Kurpanik@users.noreply.github.com>
2021-03-22 12:17:31 +0100

    Add announcementBar for Licht2021

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,13 @@ module.exports = {
   organizationName: 'globallightingdata',
   projectName: 'website',
   themeConfig: {
+    announcementBar: {
+      id: 'licht2021',
+      content: 'Visit us @<a target="_blank" rel="noopener noreferrer" href="/blog/2021/03/22/licht-2021">LICHT2021</a>',
+      backgroundColor: '#fafbfc',
+      textColor: '#091E42',
+      isCloseable: true,
+    },
     prism: {
       defaultLanguage: 'xml',
       additionalLanguages: ['csharp'],
