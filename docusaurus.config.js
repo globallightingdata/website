5d34172ca266bad4e87e8233b7061610f7fd89c1
Kurpanik <71767601+Kurpanik@users.noreply.github.com>
2022-08-23 16:03:03 +0200

    Update docusaurus.config.js to new style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,11 @@
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/palenight");
+const darkCodeTheme = require("prism-react-renderer/themes/palenight");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: "GLDF",
   tagline: "Global Lighting Data Format",
   url: "https://gldf.io",
@@ -9,168 +15,24 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "globallightingdata",
   projectName: "website",
-  themeConfig: {
-    // announcementBar: {
-    //   id: 'licht2021',
-    //   content: 'Meet us online @<a target="_blank" rel="noopener noreferrer" href="/blog/2021/03/22/licht-2021">LICHT2021</a>',
-    //   backgroundColor: '#fafbfc',
-    //   textColor: '#091E42',
-    //   isCloseable: true,
-    // },
-    docs: {
-      sidebar: {
-        hideable: true,
-        autoCollapseCategories: true,
-      }
-    },
-    prism: {
-      defaultLanguage: "xml",
-      additionalLanguages: ["csharp"],
-      /* Themes: https://github.com/FormidableLabs/prism-react-renderer/tree/master/src/themes */
-      theme: require("prism-react-renderer/themes/palenight"),
-      darkTheme: require("prism-react-renderer/themes/palenight"),
-    },
-    navbar: {
-      title: "GLDF",
-      logo: {
-        alt: "GLDF Logo",
-        src: "img/logo.png",
-        srcDark: "img/logo-dark.png",
-      },
-      items: [
-        {
-          to: "blog/",
-          label: "Blog",
-          position: "left",
-        },
-        {
-          to: "getting-started/",
-          label: "Getting Started",
-          position: "left",
-        },
-        {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Docs",
-          position: "left",
-        },
-        {
-          href: "/xsd-reference/index_Root.html",
-          label: "XSD Reference",
-          position: "left",
-          target: "_blank",
-        },
-        {
-          to: "tools/",
-          label: "Tools",
-          position: "left",
-        },
-        // {
-        //   type: 'localeDropdown',
-        //   position: 'right',
-        //   dropdownItemsBefore: [],
-        //   dropdownItemsAfter: [
-        //     {
-        //       // to: 'https://gldf.io/help-us-translate',
-        //       // label: 'Help us translate',
-        //     },
-        //   ],
-        // },
-        // {
-        //   type: 'docsVersionDropdown',
-        //   position: 'right',
-        //   dropdownActiveClassDisabled: true,
-        //   dropdownItemsBefore: [],
-        //   dropdownItemsAfter: [
-        //     {
-        //       to: '/versions',
-        //       label: 'All versions',
-        //     },
-        //   ],
-        // },
-        {
-          to: "download",
-          label: "Download",
-          position: "right",
-        },
-        // {
-        //   href: 'https://github.com/globallightingdata/gldf/releases',
-        //   label: 'Download',
-        //   position: 'right',
-        // },
-        {
-          href: "https://github.com/globallightingdata/gldf",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "DIAL GmbH",
-          items: [
-            {
-              label: "Website",
-              href: "https://www.dial.de",
-            },
-            {
-              label: "DIALux",
-              href: "https://www.dialux.com",
-            },
-            {
-              label: "LUMsearch",
-              href: "https://lumsearch.com",
-            },
-          ],
-        },
-        {
-          title: "RELUX Informatik AG",
-          items: [
-            {
-              label: "Website",
-              href: "https://relux.com",
-            },
-            {
-              label: "ReluxDesktop",
-              href: "https://relux.com/en/relux-desktop.html",
-            },
-            {
-              label: "ReluxNet",
-              href: "https://relux.com/en/search/luminaires/",
-            },
-          ],
-        },
-        {
-          title: "Imprint",
-          items: [
-            {
-              label: "Legal notice",
-              to: "imprint",
-            },
-            {
-              label: "Data Protection Policy",
-              to: "privacy-policy",
-            },
-          ],
-        },
-      ],
-      copyright: `
-      © ${new Date().getFullYear()} DIAL GmbH & RELUX Informatik AG. All rights reserved.<br />
-      Created with <a href ='https://github.com/facebook/docusaurus' target='_blank'>Docusaurus</a>.
-      `,
-    },
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
   },
+
   presets: [
     [
-      "@docusaurus/preset-classic",
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/globallightingdata/website/tree/master",
         },
         blog: {
           showReadingTime: true,
+          editUrl: "https://github.com/globallightingdata/website/tree/master",
         },
         theme: {
           customCss: [
@@ -179,13 +41,175 @@ module.exports = {
             require.resolve("./src/css/imprint.css"),
           ],
         },
-      },
+      }),
     ],
   ],
-  // scripts: [
-  //   {
-  //     src: 'https://s.imgur.com/min/embed.js',
-  //     async: true,
-  //   },
-  // ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // announcementBar: {
+      //   id: 'licht2021',
+      //   content: 'Meet us online @<a target="_blank" rel="noopener noreferrer" href="/blog/2021/03/22/licht-2021">LICHT2021</a>',
+      //   backgroundColor: '#fafbfc',
+      //   textColor: '#091E42',
+      //   isCloseable: true,
+      // },
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+      navbar: {
+        title: "GLDF",
+        logo: {
+          alt: "GLDF Logo",
+          src: "img/logo.png",
+          srcDark: "img/logo-dark.png",
+        },
+        items: [
+          {
+            to: "blog/",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            to: "getting-started/",
+            label: "Getting Started",
+            position: "left",
+          },
+          {
+            to: "docs/",
+            activeBasePath: "docs",
+            label: "Docs",
+            position: "left",
+          },
+          {
+            href: "/xsd-reference/index_Root.html",
+            label: "XSD Reference",
+            position: "left",
+            target: "_blank",
+          },
+          {
+            to: "tools/",
+            label: "Tools",
+            position: "left",
+          },
+
+          /* Lokalisierung */
+          // {
+          //   type: "localeDropdown",
+          //   position: "right",
+          //   dropdownItemsBefore: [],
+          //   dropdownItemsAfter: [
+          //     {
+          //       to: "https://gldf.io/help-us-translate",
+          //       label: "Help us translate",
+          //     },
+          //   ],
+          // },
+
+          /* Versionierung */
+          // {
+          //   type: "docsVersionDropdown",
+          //   position: "right",
+          //   dropdownActiveClassDisabled: true,
+          //   dropdownItemsBefore: [],
+          //   dropdownItemsAfter: [
+          //     {
+          //       to: "/versions",
+          //       label: "All versions",
+          //     },
+          //   ],
+          // },
+          
+          {
+            to: "download",
+            label: "Download",
+            position: "right",
+          },
+          {
+            href: "https://github.com/globallightingdata/gldf/releases",
+            label: "Releases",
+            position: "right",
+          },
+          {
+            href: "https://github.com/globallightingdata/gldf",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "DIAL GmbH",
+            items: [
+              {
+                label: "Website",
+                href: "https://www.dial.de",
+              },
+              {
+                label: "DIALux",
+                href: "https://www.dialux.com",
+              },
+              {
+                label: "LUMsearch",
+                href: "https://lumsearch.com",
+              },
+            ],
+          },
+          {
+            title: "RELUX Informatik AG",
+            items: [
+              {
+                label: "Website",
+                href: "https://relux.com",
+              },
+              {
+                label: "ReluxDesktop",
+                href: "https://relux.com/en/relux-desktop.html",
+              },
+              {
+                label: "ReluxNet",
+                href: "https://relux.com/en/search/luminaires/",
+              },
+            ],
+          },
+          {
+            title: "Imprint",
+            items: [
+              {
+                label: "Legal notice",
+                to: "imprint",
+              },
+              {
+                label: "Data Protection Policy",
+                to: "privacy-policy",
+              },
+            ],
+          },
+        ],
+        copyright: `
+      © ${new Date().getFullYear()} DIAL GmbH & RELUX Informatik AG. All rights reserved.<br />
+      Created with <a href ='https://github.com/facebook/docusaurus' target='_blank'>Docusaurus</a>.
+      `,
+      },
+      prism: {
+        defaultLanguage: "xml",
+        additionalLanguages: ["csharp"],
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // scripts: [
+      //   {
+      //     src: 'https://s.imgur.com/min/embed.js',
+      //     async: true,
+      //   },
+      // ],
+    }),
 };
+
+module.exports = config;
