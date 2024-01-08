03c81473595bc8724fb6ba71cf661b923492e868
Kurpanik <71767601+Kurpanik@users.noreply.github.com>
2024-01-08 13:13:07 +0100

    Change onBrokenLinks from throw to warn
    False positive for broken links to the XSD reference can't be fixed for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   tagline: "Global Lighting Data Format",
   url: "https://gldf.io",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "globallightingdata",
