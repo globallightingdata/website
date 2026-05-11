0bd9fce2072ef8f0f838ea9485412c1257b17eea
Alex Kurpanik <71767601+Kurpanik@users.noreply.github.com>
2026-05-11 14:03:03 +0200

    Update docusaurus.config.js to 3.10.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,22 +1,20 @@
 // @ts-check
-
-import { themes as prismThemes } from "prism-react-renderer";
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "GLDF",
-  tagline: "Global Lighting Data Format",
-  url: "https://gldf.io",
-  baseUrl: "/",
-  favicon: "img/favicon.ico",
-  organizationName: "globallightingdata",
-  projectName: "website",
-  future: { v4: true, },
+  title: 'GLDF',
+  tagline: 'Global Lighting Data Format',
+  favicon: 'img/favicon.ico',
 
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+  // Set the production url of your site here
+  url: 'https://gldf.io',
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  organizationName: 'globallightingdata',
+  projectName: 'website',
 
   markdown: {
     hooks: {
@@ -25,22 +23,31 @@ const config = {
     },
   },
 
+  future: {
+    v4: true,
+  },
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: "./sidebars.js",
-          editUrl: "https://github.com/globallightingdata/website/tree/master",
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/globallightingdata/website/tree/master',
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ["rss", "atom"],
+            type: ['rss', 'atom'],
             xslt: true,
           },
-          editUrl: "https://github.com/globallightingdata/website/tree/master",
+          editUrl: 'https://github.com/globallightingdata/website/tree/master',
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -48,9 +55,9 @@ const config = {
         },
         theme: {
           customCss: [
-            "./src/css/custom.css",
-            "./src/css/index.css",
-            "./src/css/imprint.css",
+            './src/css/custom.css',
+            './src/css/index.css',
+            './src/css/imprint.css',
           ],
         },
       }),
@@ -60,13 +67,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // announcementBar: {
-      //   id: 'licht2021',
-      //   content: 'Meet us online @<a target="_blank" rel="noopener noreferrer" href="/blog/2021/03/22/licht-2021">LICHT2021</a>',
-      //   backgroundColor: '#fafbfc',
-      //   textColor: '#091E42',
-      //   isCloseable: true,
-      // },
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       docs: {
         sidebar: {
           hideable: true,
@@ -74,171 +78,127 @@ const config = {
         },
       },
       algolia: {
-        appId: "CZKT0F0YIB",
-        apiKey: "472ed677500d234a8afefdb39c5c54b7",
-        indexName: "gldf",
+        appId: 'CZKT0F0YIB',
+        apiKey: '472ed677500d234a8afefdb39c5c54b7',
+        indexName: 'gldf',
         contextualSearch: true,
         searchParameters: {},
-        searchPagePath: "search",
-        // Optional: Replace parts of the item URLs from Algolia.
-        // Useful when using the same search index for multiple deployments using a different baseUrl.
-        // You can use regexp or string in the `from` param.
-        // For example: localhost: 3000 vs myCompany.com / docs
-        // replaceSearchResultPathname: {
-        //   from: "/docs/", // or as RegExp: /\/docs\//
-        //   to: "/",
-        // },
+        searchPagePath: 'search',
       },
       navbar: {
-        title: "GLDF",
+        title: 'GLDF',
         logo: {
-          alt: "GLDF Logo",
-          src: "img/logo.png",
-          srcDark: "img/logo-dark.png",
+          alt: 'GLDF Logo',
+          src: 'img/logo.png',
+          srcDark: 'img/logo-dark.png',
         },
         items: [
           {
-            to: "blog/",
-            label: "Blog",
-            position: "left",
+            to: 'blog/',
+            label: 'Blog',
+            position: 'left',
           },
           {
-            to: "getting-started/",
-            label: "Getting Started",
-            position: "left",
+            to: 'getting-started/',
+            label: 'Getting Started',
+            position: 'left',
           },
           {
-            to: "docs/",
-            activeBasePath: "docs",
-            label: "Docs",
-            position: "left",
+            to: 'docs/',
+            activeBasePath: 'docs',
+            label: 'Docs',
+            position: 'left',
           },
           {
-            href: "/xsd-reference/index_Root.html",
-            label: "XSD Reference",
-            position: "left",
-            target: "_blank",
+            href: '/xsd-reference/index_Root.html',
+            label: 'XSD Reference',
+            position: 'left',
+            target: '_blank',
           },
           {
-            to: "l3d-editor/",
-            label: "L3D Editor",
-            position: "left",
-          },
-          /* Lokalisierung */
-          // {
-          //   type: "localeDropdown",
-          //   position: "right",
-          //   dropdownItemsBefore: [],
-          //   dropdownItemsAfter: [
-          //     {
-          //       to: "https://gldf.io/help-us-translate",
-          //       label: "Help us translate",
-          //     },
-          //   ],
-          // },
-
-          /* Versionierung */
-          // {
-          //   type: "docsVersionDropdown",
-          //   position: "right",
-          //   dropdownActiveClassDisabled: true,
-          //   dropdownItemsBefore: [],
-          //   dropdownItemsAfter: [
-          //     {
-          //       to: "/versions",
-          //       label: "All versions",
-          //     },
-          //   ],
-          // },
-          {
-            to: "download",
-            label: "Download",
-            position: "right",
+            to: 'l3d-editor/',
+            label: 'L3D Editor',
+            position: 'left',
           },
           {
-            type: "search",
-            position: "right",
+            to: 'download',
+            label: 'Download',
+            position: 'right',
           },
           {
-            href: "https://github.com/globallightingdata/gldf/releases",
-            label: "Releases",
-            position: "right",
+            type: 'search',
+            position: 'right',
           },
           {
-            href: "https://github.com/globallightingdata/gldf",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/globallightingdata/gldf/releases',
+            label: 'Releases',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/globallightingdata/gldf',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "DIAL GmbH",
+            title: 'DIAL GmbH',
             items: [
               {
-                label: "Website",
-                href: "https://www.dial.de",
+                label: 'Website',
+                href: 'https://www.dial.de',
               },
               {
-                label: "DIALux",
-                href: "https://www.dialux.com",
+                label: 'DIALux',
+                href: 'https://www.dialux.com',
               },
               {
-                label: "DIALux Luminaire Finder",
-                href: "https://luminaires.dialux.com",
+                label: 'DIALux Luminaire Finder',
+                href: 'https://luminaires.dialux.com',
               },
             ],
           },
           {
-            title: "RELUX Informatik AG",
+            title: 'RELUX Informatik AG',
             items: [
               {
-                label: "Website",
-                href: "https://relux.com",
+                label: 'Website',
+                href: 'https://relux.com',
               },
               {
-                label: "ReluxDesktop",
-                href: "https://relux.com/en/relux-desktop.html",
+                label: 'ReluxDesktop',
+                href: 'https://relux.com/en/relux-desktop.html',
               },
               {
-                label: "ReluxNet",
-                href: "https://relux.com/en/search/luminaires/",
+                label: 'ReluxNet',
+                href: 'https://relux.com/en/search/luminaires/',
               },
             ],
           },
           {
-            title: "Imprint",
+            title: 'Imprint',
             items: [
               {
-                label: "Legal notice",
-                to: "imprint",
+                label: 'Legal notice',
+                to: 'imprint',
               },
               {
-                label: "Data Protection Policy",
-                to: "privacy-policy",
+                label: 'Data Protection Policy',
+                to: 'privacy-policy',
               },
             ],
           },
         ],
-        copyright: `
-      © ${new Date().getFullYear()} DIAL GmbH & RELUX Informatik AG. All rights reserved.<br />
-      Created with <a href ='https://github.com/facebook/docusaurus' target='_blank'>Docusaurus</a>.
-      `,
+        copyright: `© ${new Date().getFullYear()} DIAL GmbH & RELUX Informatik AG. All rights reserved.<br />Built with Docusaurus.`,
       },
       prism: {
-        defaultLanguage: "xml",
-        additionalLanguages: ["csharp"],
+        additionalLanguages: ['csharp'],
         theme: prismThemes.palenight,
         darkTheme: prismThemes.palenight,
       },
-      // scripts: [
-      //   {
-      //     src: 'https://s.imgur.com/min/embed.js',
-      //     async: true,
-      //   },
-      // ],
     }),
 };
 
